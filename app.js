72ccfba47c3460ba4170afaaf00ca88f66bfbe8a
nzilla <victorbliss044@gmail.com>
2025-05-16 23:51:28 +0100

    commit

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
+
 // ✅ Swagger UI MUST be declared BEFORE other routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
